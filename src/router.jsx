b1aff87caa3871d663418ee5d07771dead736926
wonheeki <wonluvv229@gmail.com>
2024-12-28 10:22:37 +0900

    feat : zustand를 사용한 사용자 인증 관리

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
-import { pageRoutes } from "./apiRoutes";
-import MainPage from "./pages";
-import ErrorPage from "./pages/error/components/ErrorPage";
-import LoginPage from "./pages/login";
-import RegisterPage from "./pages/register/components";
-import ProductPage from "./pages/products";
-import CommonLayout from "./pages/common/components/CommonLayout";
-import CartPage from "./pages/carts/components";
+import { createBrowserRouter } from 'react-router-dom';
+import { pageRoutes } from './apiRoutes';
+import MainPage from './pages';
+import ErrorPage from './pages/error/components/ErrorPage';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register/components';
+import ProductPage from './pages/products';
+import CommonLayout from './pages/common/components/CommonLayout';
+import CartPage from './pages/carts/components';
+import Mypage from './pages/mypage/components';
 
 // const CommonLayout = () => (
 //     <RootErrorBoundary>
@@ -17,42 +18,48 @@ import CartPage from "./pages/carts/components";
 //     </RootErrorBoundary>
 //   );
 
-const router= createBrowserRouter([
-    {
-        element:<CommonLayout/>,
-        children:[
-            {
-                path: pageRoutes.main,
-                element:<MainPage/>,
-                errorElement: <ErrorPage/>,
-            }, {
-                path: pageRoutes.register,
-                element:<RegisterPage/>,
-                errorElement: <ErrorPage/>,
-            }, {
-                path: pageRoutes.login,
-                element:<LoginPage/>,
-                errorElement: <ErrorPage/>,
-            },
-            {
-                path: pageRoutes.product,
-                element:<ProductPage/>,
-                errorElement: <ErrorPage/>,
-            },
-            {
-                path: pageRoutes.cart,
-                element:<CartPage/>,
-                errorElement: <ErrorPage/>,
-            },
-            
-            // {
-            //     path: pageRoutes.callback,
-            //     element: <AuthCallbackHandler />, // 필요한 컴포넌트 추가    
-            //     errorElement: <ErrorPage />,
-            //   }
-              
-        ],
-    },
+const router = createBrowserRouter([
+  {
+    element: <CommonLayout />,
+    children: [
+      {
+        path: pageRoutes.main,
+        element: <MainPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: pageRoutes.register,
+        element: <RegisterPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: pageRoutes.login,
+        element: <LoginPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: pageRoutes.product,
+        element: <ProductPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: pageRoutes.cart,
+        element: <CartPage />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: pageRoutes.mypage,
+        element: <Mypage />,
+        errorElement: <ErrorPage />,
+      },
+
+      // {
+      //     path: pageRoutes.callback,
+      //     element: <AuthCallbackHandler />, // 필요한 컴포넌트 추가
+      //     errorElement: <ErrorPage />,
+      //   }
+    ],
+  },
 ]);
 
 export default router;
