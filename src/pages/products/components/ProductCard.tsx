5cac8470d1c2dafe299674ad0032ef41f7c32ef2
wonheeki <wonluvv229@gmail.com>
2025-01-02 00:33:49 +0900

    feat : 장바구니 추가 로직 작성 완료

--- src/pages/products/components/ProductCard.tsx
+++ src/pages/products/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import supabase from '../../../supabase';
 import { IProduct } from '@/types/dto/productDTO';
 import { useNavigate } from 'react-router-dom';
+import { BiHeart, BiSolidCommentDetail, BiSolidHeart } from 'react-icons/bi';
 
 const ProductCardComponent = () => {
   const [data, setData] = useState<IProduct[]>([]);
@@ -11,7 +12,7 @@ const ProductCardComponent = () => {
   const fetchProduct = async () => {
     const { data } = await supabase.from('products').select();
     setData(data || []);
-    console.log(data);
+    // console.log(data);
   };
 
   const moveToDetailPage = (id: string) => {
@@ -27,16 +28,34 @@ const ProductCardComponent = () => {
           onClick={() => {
             moveToDetailPage(item.id);
           }}
-          className="w-[48%]"
+          className="w-[48%] mb-3 relative"
           key={item.id}
         >
-          <img src="" alt="원두사진" className="" />
-          <div>{item.name}</div>
-          <div>{item.flavor}</div>
-          <div>{item.price}</div>
-          <div>
-            <div>찜 아이콘 개수</div>
-            <div>리뷰 아이콘 개수</div>
+          <img
+            src={item.image_url}
+            alt="원두사진"
+            className="w-full h-[290px]"
+          />
+          <BiHeart className="absolute bottom-[8.5rem] right-2 text-2xl text-white z-50 cursor-pointer" />
+          <div className="pt-3">{item.name}</div>
+          <div className="font-medium mt-1">{item.price}원</div>
+          {item.flavor === '고소함' ? (
+            <span className="text-xs mr-1 pt-[0.2rem] pb-[0.15rem] px-2 font-bold text-customBlack rounded bg-clipColor1">
+              {item.flavor}
+            </span>
+          ) : (
+            <span className="text-xs mr-1 pt-[0.2rem] pb-[0.15rem] px-2 font-bold text-customBlack rounded bg-clipColor2">
+              {item.flavor}
+            </span>
+          )}
+
+          <div className="flex items-center text-xs text-gray-400 font-medium py-3">
+            <div className="pr-2">
+              <BiSolidHeart className="inline " /> 0
+            </div>
+            <div>
+              <BiSolidCommentDetail className="inline" /> 0
+            </div>
           </div>
           {/* <button onClick={fetchData}>확인</button> */}
         </div>
--- src/pages/products/components/ProductDetail.tsx
+++ src/pages/products/components/ProductDetail.tsx
@@ -1,13 +1,17 @@
+import useAuthStore from '@/stores/auth/useAuthStore';
 import supabase from '@/supabase';
 import { IProduct } from '@/types/dto/productDTO';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { BiHeart, BiSolidCommentDetail, BiSolidHeart } from 'react-icons/bi';
 
 const ProductDetailPage = () => {
   const { id: productId } = useParams();
+  const { user } = useAuthStore();
 
   const [product, setProduct] = useState<IProduct | null>(null);
 
+  // 상품 데이터 가져오기
   const fetchProductData = async () => {
     const { data, error } = await supabase
       .from('products')
@@ -22,34 +26,98 @@ const ProductDetailPage = () => {
   };
   useEffect(() => {
     fetchProductData();
-    // console.log(productId);
-    // console.log(product);
   }, []);
 
+  // 상품 개수 상태
   const [count, setCount] = useState(1);
+
+  // 상품 개수 갱신 함수
   const handleCountChange = (quantity: string) => {
     setCount(Number(quantity));
   };
 
+  // 장바구니 추가 로직
+  const addCartBtn = async () => {
+    if (!user?.id || !product?.id) {
+      console.error('유효한 사용자 또는 상품 정보가 없습니다.');
+      return;
+    }
+
+    const cartData = {
+      user_id: user?.id,
+      product_id: product?.id,
+      quantity: count,
+      price: product?.price,
+    };
+
+    const { data } = await supabase
+      .from('cart')
+      .select()
+      .eq('user_id', user?.id)
+      .eq('product_id', product?.id)
+      .single();
+
+    if (data) {
+      const newQuantity = count + data.quantity;
+      const { error: updateError } = await supabase
+        .from('cart')
+        .update({ quantity: newQuantity })
+        .eq('product_id', product?.id)
+        .eq('user_id', user?.id);
+      if (updateError) {
+        console.error('장바구니 업데이트 중 에러 발생:', updateError.message);
+      } else {
+        console.log('장바구니가 업데이트되었습니다.');
+      }
+    } else {
+      const { error: insertError } = await supabase
+        .from('cart')
+        .insert(cartData);
+
+      if (insertError) {
+        console.error('장바구니 추가 중 에러 발생:', insertError.message);
+      } else {
+        console.log('장바구니가 추가되었습니다.');
+      }
+    }
+  };
+
   return (
     <>
-      <div className="flex p-4">
-        <div>
-          <img
-            className="border border-emerald-50 w-[50%] g-50%]"
-            alt="원두이미지"
-            src={product?.image_url}
-          />
-        </div>
-        <div>
-          <div>{product?.name}</div>
-          <div>{product?.origin}</div>
-          <div>{product?.flavor}</div>
-          <div>{product?.price}</div>
-          <div>{product?.description}</div>
+      <div className="">
+        <img
+          // className="border border-emerald-50 w-[50%] g-50%]"
+          className="w-full"
+          alt="원두이미지"
+          src={product?.image_url}
+        />
+        <div className="p-4">
+          <div className="flex justify-between items-center">
+            <div className="text-xs ">
+              <span className="mr-1 py-1 px-2 font-bold rounded bg-clipColor2 ">
+                {product?.origin}
+              </span>
+              <span className="py-1 px-2 bg-clipColor1 font-bold">
+                {product?.flavor}
+              </span>
+            </div>
+            <BiHeart className=" text-2xl text-gray-400 z-50 cursor-pointer" />
+          </div>
+          <div className="py-4"> {product?.name}</div>
+          <div className="line-through text-sm text-gray-400">
+            {product?.price}원
+          </div>
+          <div className="text-sm font-semibold text-pointColor">
+            원두 구독시 최대 할인 가격
+          </div>
+          <div className=" font-medium text-2xl">
+            <span className=" text-pointColor">20%</span> &nbsp;
+            {Math.floor(((Number(product?.price) ?? 0) * 0.8) / 100) * 100}
+            <span className="text-sm font-semibold">원</span>
+          </div>
         </div>
       </div>
-      <div className="p-4">
+      <div className="p-4 justify-between flex">
         <input
           type="number"
           value={count}
@@ -59,8 +127,19 @@ const ProductDetailPage = () => {
             handleCountChange(e.target.value);
           }}
         />
-        <button>장바구니</button>
-        <button>바로구매</button>
+        <button
+          className="w-[240px] p-2 text-xs border border-solid text-customBlack font-bold border-gray-200 rounded-none"
+          onClick={addCartBtn}
+        >
+          장바구니 담기
+        </button>
+        <button className="w-[240px] border text-xs border-solid bg-customBlack rounded-none text-white font-bold">
+          바로 구매하기
+        </button>
+      </div>
+      <div>
+        원산지 맛설명 상품설명
+        <div>{product?.description}</div>
       </div>
       <div className="p-4">리뷰요</div>
     </>
