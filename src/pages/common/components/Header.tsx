bbad2305715b950d9b49aae01fbb1f1c66558390
wonheeki <wonluvv229@gmail.com>
2025-01-15 01:21:54 +0900

    feat: 헤더 장바구니 아이콘에 상품 개수 배지 추가

--- src/pages/common/components/Header.tsx
+++ src/pages/common/components/Header.tsx
@@ -1,8 +1,10 @@
+import { useCartItems } from '@/hooks/cart/useCartItems';
 import { ShoppingBagIcon, User } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
+  const { data } = useCartItems();
   const goToProductPage = () => {
     navigate('/cart');
   };
@@ -12,6 +14,7 @@ const Header = () => {
   const goToMyPage = () => {
     navigate('/mypage');
   };
+  const totalCartCount = data?.length;
   return (
     <div className=" h-[60px] bg-transparent w-[600px] m-0 p-4 flex items-center justify-between bg-gray-100">
       <img
@@ -22,10 +25,19 @@ const Header = () => {
       />
       <div className="flex">
         <User onClick={goToMyPage} className="h-6 w-6 text-customBlack" />
-        <ShoppingBagIcon
-          onClick={goToProductPage}
-          className="h-6 w-6 text-customBlack ml-5"
-        />
+        <div className="relative">
+          <ShoppingBagIcon
+            onClick={goToProductPage}
+            className="h-6 w-6 text-customBlack ml-5"
+          />
+          {totalCartCount === 0 ? (
+            <></>
+          ) : (
+            <div className="rounded-2xl w-[18px] h-[18px] bg-pointColor absolute top-[-5px] right-[-5px] text-center text-white leading-[18px] text-xs">
+              {totalCartCount}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
